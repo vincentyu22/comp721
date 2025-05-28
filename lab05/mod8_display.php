3925b297eb254b5b3654e6d8e997e727346ece6b
pui ming vicnent yu <132400993+vincentyu22@users.noreply.github.com>
2025-05-28 23:52:56 +1200

    Update mod8_display.php

--- lab05/mod8_display.php
+++ lab05/mod8_display.php
@@ -8,6 +8,7 @@
 	// sql info or use include 'file.inc'
 require_once ("../../files/settings.php");
 
+
 $conn = @mysqli_connect($sql_host,
 		$sql_user,
 		$sql_pass,
@@ -18,33 +19,30 @@ $conn = @mysqli_connect($sql_host,
 	if (!$conn) {
 		// Displays an error message
 		echo "<p>Database connection failure</p>";
-	} else{ 
-		$query = "SELECT car_id, make, model, price FROM car";
+	} else
+	{ 
+	$query = "SELECT car_id, make, model, price FROM car";
 			
 		// executes the query and store result into the result pointer
-		$result = mysqli_query($conn, $query);
+	$result = mysqli_query($conn, $query);
 		
 		// checks if the execuion was successful
 		if(!$result) {
-			echo "<p>Something is wrong with ". mysqli_error($conn) "</p>";
-		} elseif(mysqli_num_row){
-			echo "<p>yesssss"</p>";
-			// Display the retrieved records
-			echo "<table border=\"1\">";
-			echo "<tr>\n"
-				 ."<th scope=\"col\">ID</th>\n"
-			     ."<th scope=\"col\">Make</th>\n"
-				 ."<th scope=\"col\">Model</th>\n"
-				 ."<th scope=\"col\">Price</th>\n"
-				 ."</tr>\n";
-			while ($row = mysqli_fetch_assoc($result)){
-				echo "<tr>";
-				echo "<td>{$row['car_id']}</td>";
-				echo "<td>{$row['make']}</td>";
-				echo "<td>{$row['model']}</td>";
-				echo "<td>{$row['price']}</td>";
-				echo "</tr>";
-			}
+			echo "<p>Something is wrong with ". mysqli_error($conn) ."</p>";
+		} elseif (mysqli_num_rows($result) > 0) 
+		{
+			 
+			
+			echo "<table>";
+        echo "<tr><th>Car ID</th><th>Make</th><th>Model</th><th>Price ($)</th></tr>";
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<td>{$row['car_id']}</td>";
+            echo "<td>{$row['make']}</td>";
+            echo "<td>{$row['model']}</td>";
+            echo "<td>{$row['price']}</td>";
+            echo "</tr>";
+        }
 			echo "</table>";
 			
 		} else 
@@ -52,7 +50,7 @@ $conn = @mysqli_connect($sql_host,
 
 echo "<p>no record found</p>";
 }		
-	
+	 mysqli_close($conn);
 
 ?>
 </body>
