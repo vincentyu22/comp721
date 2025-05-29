4471faaff8f5ab6fc1df56b2d6d5794ede8520e6
pui ming vicnent yu <132400993+vincentyu22@users.noreply.github.com>
2025-05-29 12:52:46 +1200

    Update simpleajax.js

--- lab07/fetch/simpleajax.js
+++ lab07/fetch/simpleajax.js
@@ -1,10 +1,29 @@
 // file simpleajax.js
 // var xhr = createRequest();
 function getData(dataSource, divID, aName, aPwd)  {
-  
-	var place = document.getElementById(divID);
-	var url = dataSource+"?name="+aName+"&pwd="+aPwd;
+	const place = document.getElementById(divID);
+	vconst data = new URLSearchParams();
+data.append('name', aName);
+data.append('pwd', aPwd);
 
+    place.innerHTML = "<em>Fetching data... please wait.</em>";
+fetch(dataSource, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: data.toString() })
+    .then(response => response.json())
+    .then(result => 
+            if (result.status === 'ok') {  place.innerHTML = `${result.name} email address <em>${result.email}</em>`;} else {
+                alert(result.message);
+                place.innerHTML = '';}
+        
+    })
+    .catch(error => {alert('Fetch error: ' + error);
+		      place.innerHTML.jas = ''; });
+}
+
+
+/*	
 	const requestPromise = fetch(url);
 	requestPromise.then(
 		function (response){
@@ -12,6 +31,12 @@ function getData(dataSource, divID, aName, aPwd)  {
 				place.innerHTML = text;
 			});
 
+
+
+			
 		}
 	);
 } 
+*?
+
+
